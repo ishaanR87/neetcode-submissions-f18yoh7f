6fea5ee805752e69195710f357375c99876e65fa
ishaanR87 <55754104+ishaanR87@users.noreply.github.com>
2026-05-28 13:40:10 +0100

    Add: find-anagram-mappings - submission-0

---
+++ Data Structures & Algorithms/find-anagram-mappings/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] anagramMappings(int[] nums1, int[] nums2) {
+        
+        int[] res = new int[nums1.length];
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i], i);
+        }
+
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.get(nums1[i]);
+        }
+
+        return res;
+    }
+}
